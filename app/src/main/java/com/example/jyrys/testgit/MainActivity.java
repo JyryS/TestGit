8baa3d614757527b8f1801940bd19e60ef2fa0e7
JyryS <jyrysoinio@yahoo.com>
2018-09-27 09:41:25 +0300

    Complicated stuff

--- app/src/main/java/com/example/jyrys/testgit/MainActivity.java
+++ app/src/main/java/com/example/jyrys/testgit/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
+    }
+
+    public void testi() {
+        int i = 1;
     }
 }
